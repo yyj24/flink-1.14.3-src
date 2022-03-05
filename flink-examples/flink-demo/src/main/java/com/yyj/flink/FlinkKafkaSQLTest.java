d5cbdd8f299b596c877c07baeb3e142f8d596ece
yyj8 <1012293987@qq.com>
2022-03-05 20:08:30 +0800

    添加flink sql kafka测试代码

--- flink-examples/flink-demo/src/main/java/com/yyj/flink/FlinkKafkaSQLTest.java
+++ flink-examples/flink-demo/src/main/java/com/yyj/flink/FlinkKafkaSQLTest.java
@@ -4,11 +4,12 @@ import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.table.api.bridge.java.StreamTableEnvironment;
 
 public class FlinkKafkaSQLTest {
+
     public static void main(String[] args) throws Exception {
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
         StreamTableEnvironment tEnv = StreamTableEnvironment.create(env);
-        final String createTable =
-                "CREATE TABLE tb_user (\n"
+        final String createTable1 =
+                "CREATE TABLE tb_user1 (\n"
                         + "  `name` STRING,\n"
                         + "  `age` INTEGER\n"
                         + ") WITH (\n"
@@ -19,9 +20,30 @@ public class FlinkKafkaSQLTest {
                         + "  'scan.startup.mode' = 'latest-offset',\n"
                         + "  'format' = 'json'\n"
                         + ")";
-        tEnv.executeSql(createTable);
+        //创建tb_user1表
+        tEnv.executeSql(createTable1);
 
-        String query = "select * from tb_user";
+        final String createTable2 =
+                "CREATE TABLE tb_user2 (\n"
+                        + "  `name` STRING,\n"
+                        + "  `age` INTEGER\n"
+                        + ") WITH (\n"
+                        + "  'connector' = 'kafka',\n"
+                        + "  'topic' = 'yyj2',\n"
+                        + "  'properties.bootstrap.servers' = 'localhost:9092,localhost:9093,localhost:9094',\n"
+                        + "  'properties.group.id' = 'group2',\n"
+                        + "  'scan.startup.mode' = 'latest-offset',\n"
+                        + "  'format' = 'json'\n"
+                        + ")";
+        //创建tb_user2表
+        tEnv.executeSql(createTable2);
+
+        final String insert = "insert into tb_user2 select name,age from tb_user1";
+        //查询tb_user1表，插入tb_user2表
+        tEnv.executeSql(insert);
+
+        //查询tb_user2表
+        final String query = "select name,age from tb_user2";
         tEnv.toDataStream(tEnv.sqlQuery(query)).print();
 
         env.execute("FlinkKafkaSQLTest");
